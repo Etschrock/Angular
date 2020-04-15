57c8a129cac2ffde8d492c8c8cebf5423492a483
Etschrock <etschrock@yahoo.com>
2020-04-14 23:59:26 -0400

    Angular course on pluralsight. Initial commit
    This is for an Angular pluralsight course: Angular Fundamentals. Completed through module 4.

---
+++ ng-fundamentals/src/app/events-app.component.ts
@@ -0,0 +1,27 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'events-app',
+  template: `
+  <nav-bar></nav-bar>
+  <events-list></events-list>
+  `,
+  styles:[`
+    .nav .navbar-nav {
+      font-size: 15px;
+    }
+
+    #searchForm {
+      margin-right: 100px;
+    }
+
+    @media(max-width: 1200px;) {
+      #searchForm {
+        display: none;
+      }
+    }
+  `]
+})
+export class EventsAppComponent {
+  title = 'ng-fundamentals';
+}
